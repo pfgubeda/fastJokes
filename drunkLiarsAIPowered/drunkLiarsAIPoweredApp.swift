27edc12aa5b7a4cc7c99bfc6c8f8ed5fbafd9086
Pablo <pabloubeda2000@gmail.com>
2024-02-08 16:43:50 +0100

    Initial Commit

---
+++ drunkLiarsAIPowered/drunkLiarsAIPoweredApp.swift
@@ -0,0 +1,17 @@
+//
+//  drunkLiarsAIPoweredApp.swift
+//  drunkLiarsAIPowered
+//
+//  Created by Pablo Fernandez Gonzalez on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct drunkLiarsAIPoweredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
